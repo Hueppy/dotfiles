537d8ea0403e6f173517f35801134dc182343f8b
Patrick Hüntelmann <patrick.huentelmann@gmail.com>
2022-01-31 01:24:07 +0100

    add emacs config

---
+++ home/emacs.d/init.el
@@ -0,0 +1,45 @@
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+
+(defun startup/reset-gc ()
+  (setq gc-cons-threshold 16777216
+    gc-cons-percentage 0.1))
+ 
+(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
+(add-hook 'emacs-startup-hook 'startup/reset-gc)
+
+(defvar startup/file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+ 
+(defun startup/revert-file-name-handler-alist ()
+  (setq file-name-handler-alist startup/file-name-handler-alist))
+
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+        '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Initialize use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+(defun load-config-file (config-file)
+  (org-babel-load-file (expand-file-name config-file user-emacs-directory)))
+
+(load-config-file "config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(nord-theme use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
